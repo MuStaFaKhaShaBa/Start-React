08a25f8f0dcf1da36b37163b6f008cd75c6b9f07
Mostafa Maher <mostafa_khashaba30@outlook.com>
2022-11-28 07:54:59 +0200

    upload files

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar() {
-    const Path = window.location.pathname,
-        pageTitle = Path.split('').splice(1, Path.indexOf('.') - 1).join("") || 'home';
-
-
-
-    const [activeLink, setActiveLink] = useState(pageTitle);
-
-    useEffect(() => {
-        console.log('re');
-        window.document.title = activeLink;
-
-    }, [activeLink]);
 
     return (
         <>
@@ -22,7 +10,8 @@ function Navbar() {
                 <div className="container py-2">
                     <Link className="navbar-brand fs-2" to="">Start React</Link>
 
-                    <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <p className='mb-0 d-inline'>Menu</p>
                         <span className="navbar-toggler-icon ms-0 ms-md-2"></span>
                     </button>
@@ -30,16 +19,16 @@ function Navbar() {
                     <div className="collapse navbar-collapse fs-5" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 my-lg-0 mt-3">
                             <li className="nav-item my-2 my-lg-0">
-                                <Link className={`nav-link py-2 px-3 rounded-2 ${activeLink === 'home' && 'active'}`} aria-current="page" onClick={_ => setActiveLink('home')} to="">Home</Link>
+                                <NavLink className={`nav-link py-2 px-3 rounded-2`} aria-current="page" to="">Home</NavLink>
                             </li>
                             <li className="nav-item my-2 my-lg-0">
-                                <Link className={`nav-link py-2 px-3 rounded-2 ${activeLink === 'portfolio' && 'active'}`} onClick={_ => setActiveLink('portfolio')} to="portfolio.html">Portfolio</Link>
+                                <NavLink className={`nav-link py-2 px-3 rounded-2`} to="portfolio.html">Portfolio</NavLink>
                             </li>
                             <li className="nav-item my-2 my-lg-0">
-                                <Link className={`nav-link py-2 px-3 rounded-2 ${activeLink === 'about' && 'active'}`} onClick={_ => setActiveLink('about')} to="about.html">about</Link>
+                                <NavLink className={`nav-link py-2 px-3 rounded-2`} to="about.html">about</NavLink>
                             </li>
                             <li className="nav-item my-2 my-lg-0">
-                                <Link className={`nav-link py-2 px-3 rounded-2 ${activeLink === 'contact' && 'active'}`} onClick={_ => setActiveLink('contact')} to="contact.html">contact</Link>
+                                <NavLink className={`nav-link py-2 px-3 rounded-2`} to="contact.html">contact</NavLink>
                             </li>
                         </ul>
 
